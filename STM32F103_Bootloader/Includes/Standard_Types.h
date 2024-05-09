da48677bde64773911c9176edb2648f098b38683
ammarkhalil07 <ammarkhalil707@gmail.com>
2024-05-09 18:26:20 +0200

    Bootloader

---
+++ STM32F103_Bootloader/Includes/Standard_Types.h
@@ -0,0 +1,32 @@
+/*******************************************************************
+ *  FILE DESCRIPTION
+-----------------------
+ *  Author: Ammar Khalil
+ *  File: ./standard_types.h
+ *  Date: MAY 8, 2024
+ *  Module:  Library/Bootloader
+ *  Description: Standard library File
+*******************************************************************/
+#ifndef _STANDERD_TYPES_H_
+#define _STANDERD_TYPES_H_
+/*****************************************
+----------    GLOBAL DATA     -----------
+*****************************************/
+/*----------- Unsigned -----------*/
+typedef unsigned char u8;               /*Unsigned 1 byte*/
+typedef unsigned short int u16;         /*Unsigned 2 bytes*/
+typedef unsigned long int u32;          /*Unsigned 4 bytes*/
+typedef unsigned long long u64;         /*Unsigned 8 bytes*/
+/*----------- Signed -----------*/
+typedef signed char s8;                 /*signed 1 byte*/
+typedef signed short int s16;           /*signed 2 bytes*/
+typedef signed long int s32;            /*signed 4 bytes*/
+typedef signed long long s64;           /*signed 8 bytes*/
+/*----------- Float -----------*/
+typedef float f32;                      /*Float 4 bytes*/
+typedef double f64;                     /*Float 8 bytes*/
+typedef long double f128;               /*Float 16 bytes*/
+/********************************************************************
+ *  END OF FILE: Standard_Types.h
+********************************************************************/
+#endif
