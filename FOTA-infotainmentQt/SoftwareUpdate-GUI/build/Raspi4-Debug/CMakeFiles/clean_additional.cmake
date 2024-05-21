bd9463d6fe8615c7feb446271678917350052963
som3a13 <134228578+som3a13@users.noreply.github.com>
2024-05-21 20:05:26 +0300

    UPDATE

---
+++ FOTA-infotainmentQt/SoftwareUpdate-GUI/build/Raspi4-Debug/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,10 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
+  file(REMOVE_RECURSE
+  "CMakeFiles/Raspi4_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/Raspi4_autogen.dir/ParseCache.txt"
+  "Raspi4_autogen"
+  )
+endif()
